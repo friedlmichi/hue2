d1566e6846bada952bb484caa96960709c6cf8ab
Michael Friedl <friedlm1608@gmail.com>
2024-09-24 11:26:49 +0200

    main methdoe wurde implementiert, sowie der erste lamda ausdruck zum printen der Liste

--- hue2/src/HalloJavamitForEach.java
+++ hue2/src/HalloJavamitForEach.java
@@ -1,4 +1,16 @@
-public class HalloJavamitForEach {
-     
+import java.util.ArrayList;
+import java.util.List;
 
+public class HalloJavamitForEach {
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (String s : list) {
+            System.out.println(s);
+        }
+        list.forEach((String s) -> System.out.println(s));
+
+
+
+    }
 }
