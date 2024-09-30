c011ab882128d32da79c3d6eab55952fb928a095
Michael Friedl <friedlm1608@gmail.com>
2024-09-30 19:55:08 +0200

    Klasse erstellet und HalloJavamitForEach.java methode erstellt

--- hue2/src/HalloJavamitForEach.java
+++ hue2/src/HalloJavamitForEach.java
@@ -1,19 +1,12 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class HalloJavamitForEach {
-    static List<String> list = new ArrayList<>();
-
     public static void main(String[] args) {
+        List<String> list = Arrays.asList("Hello", "World", "Java", "forEach");
+
         for (String s : list) {
             System.out.println(s);
         }
-        //list.forEach((String s) -> System.out.println(s));
-        list.forEach(System.out::println);
-
-        Consumer<String> consumer = System.out::println;
-
-
     }
 }
