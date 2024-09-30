235b7cc7e06c06245ec6b70d9fb158cd83bd0792
Michael Friedl <friedlm1608@gmail.com>
2024-09-30 20:10:24 +0200

    VectorCalculator wurde implementiert

---
+++ hue2/src/VectorCalculator.java
@@ -0,0 +1,30 @@
+public class VectorCalculator extends AbstractCalculator {
+
+    public VectorCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return add.calc(a, b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return subtract.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        double resultA = a.getA() * b.getB() - a.getB() * b.getA();
+        double resultB = a.getB() * b.getA() - a.getA() * b.getB();
+        return new Number(resultA, resultB);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+
+        double result = a.getA() * b.getA() + a.getB() * b.getB();
+        return new Number(result, 0);
+    }
+}
