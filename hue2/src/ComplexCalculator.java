f9ed08919e35075b1acb4ff2438ce68b4f57c897
Michael Friedl <friedlm1608@gmail.com>
2024-09-30 20:11:55 +0200

    ComplexCalculator.java Klasse implementiertmit

---
+++ hue2/src/ComplexCalculator.java
@@ -0,0 +1,35 @@
+public class ComplexCalculator extends AbstractCalculator {
+
+    public ComplexCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        double resultA = a.getA() + b.getA();
+        double resultB = a.getB() + b.getB();
+        return new Number(resultA, resultB);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        double resultA = a.getA() - b.getA();
+        double resultB = a.getB() - b.getB();
+        return new Number(resultA, resultB);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        double resultA = a.getA() * b.getA() - a.getB() * b.getB();
+        double resultB = a.getA() * b.getB() + a.getB() * b.getA();
+        return new Number(resultA, resultB);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        double divisor = b.getA() * b.getA() + b.getB() * b.getB();
+        double resultA = (a.getA() * b.getA() + a.getB() * b.getB()) / divisor;
+        double resultB = (a.getB() * b.getA() - a.getA() * b.getB()) / divisor;
+        return new Number(resultA, resultB);
+    }
+}
